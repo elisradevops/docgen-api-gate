7601654a1f948c46e4e3ed550002baee9dd5d5fc
Eden Zvi Schwartz <eden@korentec.co.il>
2025-05-22 16:32:40 +0300

    Revert "Merge pull request #27 from elisradevops/bug/mongo-db-major-issue"
    This reverts commit f916c2da9ad7ee9e43defa24bc645141b1272e07, reversing
    changes made to 45e03e90868cce1a827fc5e9b062d6b920bb4880.

--- mongodb-initializer/init-scripts/01-init.js
+++ mongodb-initializer/init-scripts/01-init.js
@@ -2,19 +2,24 @@
 db = db.getSiblingDB('docgen');
 print('Connected to docgen database');
 
-// Create collections
-// Check if this is first-time initialization
-const collections = db.getCollectionNames();
-if (collections.length === 0) {
-  // Only create collections and indexes if none exist
-  db.createCollection('favorites');
-  print('Created favorites collection');
-
-  // Create indexes
-  db.favorites.createIndex({ userId: 1, docType: 1, name: 1 }, { unique: true });
-  db.favorites.createIndex({ id: 1 }, { unique: true });
-} else {
-  print('Collections already exist, skipping initialization');
+// Drop existing collections to ensure clean state
+try {
+  db.favorites.drop();
+  print('Dropped existing favorites collection');
+} catch (e) {
+  print('No favorites collection to drop');
 }
+
+// Create collections
+db.createCollection('favorites');
+print('Created favorites collection');
+
+// Create indexes matching those defined in DatabaseController.ts
+db.favorites.createIndex({ userId: 1, docType: 1, name: 1 }, { unique: true });
+print('Created compound index on favorites collection');
+
+// Create index for id field
+db.favorites.createIndex({ id: 1 }, { unique: true });
+print('Created unique index on id field');
 
 print('MongoDB initialization complete!');
