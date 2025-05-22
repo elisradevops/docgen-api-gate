04af7025692f78778e23dce3ca7ce0b21791279a
Eden Zvi Schwartz <eden@korentec.co.il>
2025-05-22 16:16:43 +0300

    Test

--- mongodb-initializer/init-scripts/01-init.js
+++ mongodb-initializer/init-scripts/01-init.js
@@ -2,24 +2,19 @@
 db = db.getSiblingDB('docgen');
 print('Connected to docgen database');
 
-// Drop existing collections to ensure clean state
-try {
-  db.favorites.drop();
-  print('Dropped existing favorites collection');
-} catch (e) {
-  print('No favorites collection to drop');
-}
-
 // Create collections
-db.createCollection('favorites');
-print('Created favorites collection');
+// Check if this is first-time initialization
+const collections = db.getCollectionNames();
+if (collections.length === 0) {
+  // Only create collections and indexes if none exist
+  db.createCollection('favorites');
+  print('Created favorites collection');
 
-// Create indexes matching those defined in DatabaseController.ts
-db.favorites.createIndex({ userId: 1, docType: 1, name: 1 }, { unique: true });
-print('Created compound index on favorites collection');
-
-// Create index for id field
-db.favorites.createIndex({ id: 1 }, { unique: true });
-print('Created unique index on id field');
+  // Create indexes
+  db.favorites.createIndex({ userId: 1, docType: 1, name: 1 }, { unique: true });
+  db.favorites.createIndex({ id: 1 }, { unique: true });
+} else {
+  print('Collections already exist, skipping initialization');
+}
 
 print('MongoDB initialization complete!');
