e04adcf5159d0d118b02f95d01e652b8498854cd
Eden Zvi Schwartz <eden@korentec.co.il>
2025-06-04 16:23:47 +0300

    Avoid dropping the favorite table

--- mongodb-initializer/init-scripts/01-init.js
+++ mongodb-initializer/init-scripts/01-init.js
@@ -4,10 +4,10 @@ print('Connected to docgen database');
 
 // Drop existing collections to ensure clean state
 try {
-  db.favorites.drop();
-  print('Dropped existing favorites collection');
+  // db.favorites.drop(); // Commented out to prevent data deletion
+  print('Favorites collection will not be dropped to preserve data.');
 } catch (e) {
-  print('No favorites collection to drop');
+  print('No favorites collection to drop (or drop is commented out)');
 }
 
 // Create collections
@@ -15,7 +15,7 @@ db.createCollection('favorites');
 print('Created favorites collection');
 
 // Create indexes matching those defined in DatabaseController.ts
-db.favorites.createIndex({ userId: 1, docType: 1, name: 1 }, { unique: true });
+db.favorites.createIndex({ userId: 1, docType: 1, name: 1, teamProjectId: 1 }, { unique: true });
 print('Created compound index on favorites collection');
 
 // Create index for id field
